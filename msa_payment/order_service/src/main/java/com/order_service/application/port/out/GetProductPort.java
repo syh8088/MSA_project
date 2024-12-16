0a25406ff4d656c508cc8679952818025ddfb6ff
syh8088 <syh8088@gmail.com>
2024-12-16 21:19:09 +0900

    잘못된 프로젝트 생성 된 거 삭제
    잘못된 프로젝트 생성 된 거 삭제

--- msa_payment/order_service/src/main/java/com/order_service/application/port/out/GetProductPort.java
+++
@@ -1,8 +0,0 @@
-package com.order_service.application.port.out;
-
-import java.util.List;
-
-public interface GetProductPort {
-
-    List<ProductOutPut> selectProductListByProductNoList(List<Long> productNoList);
-}
--- msa_payment/order_service/src/main/java/com/order_service/application/port/out/PaymentCheckOutPort.java
+++
@@ -1,8 +0,0 @@
-package com.order_service.application.port.out;
-
-import com.order_service.adapter.out.persistence.entity.PaymentEvent;
-
-public interface PaymentCheckOutPort {
-
-    void insertPaymentCheckOut(PaymentEvent paymentEvent);
-}
